93d917d66774e89286b63b5432765135a59374a3
Jiaqi Guo @ NU <jacky_guojj@outlook.com>
2021-10-08 16:15:23 -0500

    Update Q2.1 d, and change the plot of other 3 figures

--- Machine Learning FFA/Homework1/2_1.py
+++ Machine Learning FFA/Homework1/2_1.py
@@ -1,6 +1,28 @@
 import mlrefined_libraries
 from mlrefined_libraries.math_optimization_library import random_method_experiments
 from mlrefined_libraries import math_optimization_library as optlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def uniform_sample(P):
+    w = np.linspace(start, stop, P)
+    g = w.T*w
+    plt.figure(figsize=(9, 6))
+    plt.plot(w, g)
+    plt.title(u" N=1 P=100 ", fontsize=16)
+    plt.xlabel('w')
+    plt.ylabel('g(w)= w^T*w')
+    plt.plot(w, w * 0, linewidth=1, linestyle='-.', color='g')
+    plt.show()
+
+# def plot_3_picture(P):
+
 
 if __name__ == '__main__':
-    random_method_experiments.random_eval_experiment()
+    N = 100
+    P = [100, 1000, 10000]
+    start = -1
+    stop = 1
+    uniform_sample(P[0])
+    # random_method_experiments.random_eval_experiment()
--- Machine Learning FFA/Homework1/3_1.py
+++ Machine Learning FFA/Homework1/3_1.py
@@ -18,49 +18,51 @@ def Single_function_plot(G_n, observe_region):
     # plt.subplot(111)  # 第一个参数表示：行，第二个参数表示；列，第三个参数；当前图例中的激活位置
     plt.xlabel(u'X value')
     plt.ylabel(u'Y value')
-    plt.title(u" First Order Plot", fontsize=16)
+    plt.title(u" G_w ", fontsize=16)
     # set the x,y limit of the output figure
     plt.xlim(observe_region[0], observe_region[1])
-    plt.ylim(-3, 3)
+    plt.ylim(-1, 5)
     # note the important point
-    plt.xticks(np.linspace(observe_region[0], observe_region[1], 9))
+    plt.xticks(np.linspace(observe_region[0], observe_region[1], 10))
     # Used fot emphasizing the
-    plt.axhline(y=0, xmax=0.5, color='r', linestyle='dashed')
-    plt.axvline(x=0, ymax=0.5, color='r', linestyle='dashed')
+    # plt.axhline(y=0, color='r', linestyle='dashed')
+    # plt.axvline(x=0.5, color='r', linestyle='dashed')
     plt.plot(X, Y_x2)
     plt.show()
 
 
-def plot_3D_functions_contour(g_n, x_range, y_range):
+def plot_3D_functions_contour(g_n):
     plt.figure()
+    Z = []
     ax3 = plt.axes(projection='3d')
-    xx = np.arange(-2, 2, 0.1)
-    yy = np.arange(-2, 2, 0.1)
-    # X, Y = np.meshgrid(xx, yy)
+    xx = np.arange(-5, 5, 0.05)
+    yy = np.arange(-5, 5, 0.05)
+    X, Y = np.meshgrid(xx, yy)
     for i in yy:
         for a in xx:
             w = np.array([[i], [a]])
-            Z = g_n(w)
+            Z.append(int(g_n(w)))
+    matrix_size = int(math.sqrt(X.size))
+    Z = np.resize(Z, (matrix_size, matrix_size))
     ax3.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='rainbow')
     plt.show()
 
 
 if __name__ == '__main__':
     # Question a
-    # G_a = lambda w: math.log(w / (1 - w))
+    # G_a = lambda w: w*math.log(w)+(1-w)*math.log(1-w)
     # Single_function_plot(G_a, [0.1, 0.99])
 
     # Question b
-    # G_b = lambda w: np.exp(w) / (1 + np.exp(w))
-    # Single_function_plot(G_b, [-50, 50])
+    # G_b = lambda w: math.log(1 + pow(math.e, w))
+    # Single_function_plot(G_b, [-50, 30])
 
     # Question c
-    # G_c = lambda w: w - w * pow(np.tanh(w), 2) + np.tanh(w)
+    # G_c = lambda w: w*math.tanh(w)
     # Single_function_plot(G_c, [-5, 5])
 
     # Question d
-    C = np.array([[2, 1], [1, 3]])
-    b = np.array([[1], [1]])
-    G_d = lambda w: 1 / 2 * np.dot((C + C.T), w) + b.T
-    plotter = static_plotter.Visualizer()
-
+    # C = np.array([[2, 1], [1, 3]])
+    # b = np.array([[1], [1]])
+    # G_d = lambda w: 1 / 2 * np.dot(np.dot(w.T, C), w) + np.dot(b.T, w)
+    # plot_3D_functions_contour(G_d)
