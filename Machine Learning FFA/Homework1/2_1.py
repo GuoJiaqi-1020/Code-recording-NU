e348bf5752540166621b0f6e2908b70e73dbbcc6
Jiaqi Guo @ NU <jacky_guojj@outlook.com>
2021-10-10 20:20:51 -0500

    modify the code of 2_1 a and b

--- Machine Learning FFA/Homework1/2_1.py
+++ Machine Learning FFA/Homework1/2_1.py
@@ -1,28 +1,41 @@
-import mlrefined_libraries
 from mlrefined_libraries.math_optimization_library import random_method_experiments
-from mlrefined_libraries import math_optimization_library as optlib
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def uniform_sample(P):
-    w = np.linspace(start, stop, P)
-    g = w.T*w
-    plt.figure(figsize=(9, 6))
-    plt.plot(w, g)
-    plt.title(u" N=1 P=100 ", fontsize=16)
-    plt.xlabel('w')
-    plt.ylabel('g(w)= w^T*w')
-    plt.plot(w, w * 0, linewidth=1, linestyle='-.', color='g')
-    plt.show()
+# def originFunction(w1):
+#     w = np.array(w1)
+#     return w ** 2
+def produceN(N):
+    axis = []
+    for i in range(N):
+        axis.append(i + 1)
+    return axis
 
-# def plot_3_picture(P):
+
+def minimum_value(N, P):
+    G_w = []
+    for i in range(N):
+        result = (i + 1) * np.linspace(-1, 1, P) ** 2
+        G_w.append(min(result))
+    return G_w
+
+
+def minimum_value_plot(X, Y, number_of_plot, label, color):
+    plt.xlabel("Dimension/N")
+    plt.ylabel("Minimum value of g(w)")
+    plt.title("Minimum value attained for each quadratic against N")
+    for i in range(number_of_plot):
+        plt.plot(X, Y[i], color=color[i], linewidth=1, label=label[i])
+    plt.legend(loc='upper left')
+    plt.show()
 
 
 if __name__ == '__main__':
     N = 100
-    P = [100, 1000, 10000]
-    start = -1
-    stop = 1
-    uniform_sample(P[0])
+    Y1 = minimum_value(100, 100)
+    Y2 = minimum_value(100, 1000)
+    Y3 = minimum_value(100, 10000)
+    minimum_value_plot(range(1, N + 1), [Y1, Y2, Y3], number_of_plot=3, label=['P=100', 'P=1000', 'P=10000'],
+                       color=['g', 'r', 'b'])
     # random_method_experiments.random_eval_experiment()
